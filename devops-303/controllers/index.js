463759cd4830743ac6057f41bfa512f8d9dc3ddf
Glenn Oberlé <oberle.glenn@icloud.com>
2022-01-11 15:49:37 +0100

    connexion redis

--- devops-303/controllers/index.js
+++ devops-303/controllers/index.js
@@ -7,7 +7,7 @@ let redis = null;
 
 try {
   redis = new Redis({
-    host: process.env.REDIS_HOST || 'redis.glenn',
+    host: process.env.REDIS_HOST || 'http://redis.glenn',
     db: process.env.REDIS_DB || 1,
     port: process.env.REDIS_PORT || 6379,
     username: process.env.REDIS_USERNAME,
